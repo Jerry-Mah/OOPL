f832afabbd76569dbf8dffbaa2dea09fada7f4b1
Jeremy Mathew <jeremyjohnmathew@gmail.com>
2023-08-29 09:56:48 +0530

    Assignment 1

---
+++ assignment1/ass1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std; 
+
+
+class Passenger { 
+ public: 
+ 	char name[50]; 
+ 	int age; 
+ 	string flightNumber;
+ 	string departureTime;
+ 	string source; 
+ 	
+ 	void storeData(){
+ 		cout << "Enter Name: " << endl;
+ 		cin.getline(name,sizeof(name));
+ 		cout << "Enter age: " << endl;
+ 		cin >> age;
+ 		cout << "Flight Number: " << endl;
+ 		cin >> flightNumber;
+ 		cout << "Departure Time: " << endl;
+ 		cin >> departureTime;
+ 		cout << "Source: " << endl;
+ 		cin >> source;
+ 		
+ 		cout << "Data has been stored";
+	 }
+ 	
+ 	void showInfo(){
+ 		cout << "age: " << age<< endl;
+ 		cout << "name: " << name<< endl;
+ 		cout << "flight number: " << flightNumber<< endl;
+ 		cout << "departure time: " << departureTime<< endl;
+ 		cout << "source: " << source<< endl;
+ 	
+	 }
+};
+
+int main(){
+	Passenger pass1;
+	pass1.storeData();
+	pass1.showInfo();
+}
